4603993d833d9b49a1d4d01826742d3b65016033
SpurthiSetty <spurthi.setty@gmail.com>
2025-03-25 21:57:45 -0400

    added app and login scrren, still working on getting it to render

---
+++ frontend/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import LoginScreen from './screens/LoginScreen'; // Adjust the path if necessary
+
+export default function App() {
+  console.log('App is rendering');
+  return <LoginScreen />;
+}
