3367105ba3a1d4abe6c77561fc143e820a92b0d5
SpurthiSetty <spurthi.setty@gmail.com>
2025-03-25 22:38:59 -0400

    added register screen

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import LoginScreen from "../screens/LoginScreen"; // Adjust the path if necessary
-
+import RegisterScreen from "../screens/RegisterScreen"; // Adjust the path if necessary
 export default function Index() {
-  return <LoginScreen />;
+  // return <LoginScreen />;
+  return <RegisterScreen />;
 }
