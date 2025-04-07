9d60d91e427458de7d187417070a22d8a31e68f2
Thomas-Ung <tung@stevens.edu>
2025-04-07 16:49:32 -0400

    connected messenger.ts to database

--- backend/messenger.js
+++ backend/messenger.js
@@ -1,37 +1,114 @@
+"use strict";
+//run this file in terminal 
+// 1st: cd backend
+// 2nd: tsc messenger.ts
+// 3rd: node messenger.js
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
+    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var admin = require("firebase-admin");
+var serviceAccount = require('C:/Users/thoma/Documents/ExchangeForStudents/exchange-for-students-firebase-adminsdk-fbsvc-55c7e57c50.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+var db = admin.firestore();
 var Message = /** @class */ (function () {
     function Message(sender, content, timestamp) {
         this.sender = sender;
         this.content = content;
         this.timestamp = timestamp;
     }
-    // __repr__ equivalent
-    Message.prototype.toString = function () {
-        return "Message from ".concat(this.sender, " at ").concat(this.timestamp, ": ").concat(this.content);
-    };
     return Message;
 }());
 var Conversation = /** @class */ (function () {
-    function Conversation(participants, product) {
+    function Conversation(participants, product, docId) {
         this.participants = participants;
         this.product = product;
-        this.messages = [];
+        this.docId = docId;
     }
     Conversation.prototype.addMessage = function (sender, content) {
-        if (this.participants.includes(sender)) {
-            var now = new Date().toISOString().slice(0, 19).replace('T', ' ');
-            var message = new Message(sender, content, now);
-            this.messages.push(message);
-            return true;
-        }
-        return false;
-    };
-    // __repr__ equivalent
-    Conversation.prototype.toString = function () {
-        var output = '';
-        this.messages.forEach(function (message) {
-            output += "".concat(message.toString(), " \n");
+        return __awaiter(this, void 0, void 0, function () {
+            var now, message, messageRef;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        if (!this.participants.includes(sender)) return [3 /*break*/, 2];
+                        now = new Date().toISOString();
+                        message = new Message(sender, content, now);
+                        messageRef = db.collection('conversations')
+                            .doc(this.docId)
+                            .collection('messages');
+                        return [4 /*yield*/, messageRef.add({
+                                sender: message.sender,
+                                content: message.content,
+                                timestamp: message.timestamp,
+                            })];
+                    case 1:
+                        _a.sent();
+                        console.log("Message added to convo ".concat(this.docId, ": ").concat(content));
+                        return [2 /*return*/, true];
+                    case 2:
+                        console.log("Sender not in participants");
+                        return [2 /*return*/, false];
+                }
+            });
         });
-        return "<Conversation between ".concat(this.participants.join(', '), ">\n").concat(output);
+    };
+    Conversation.prototype.printMessages = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var messagesSnapshot;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, db.collection('conversations')
+                            .doc(this.docId)
+                            .collection('messages')
+                            .orderBy('timestamp')
+                            .get()];
+                    case 1:
+                        messagesSnapshot = _a.sent();
+                        console.log("\nConversation about \"".concat(this.product, "\" between [").concat(this.participants.join(', '), "]:"));
+                        messagesSnapshot.forEach(function (doc) {
+                            var _a = doc.data(), sender = _a.sender, content = _a.content, timestamp = _a.timestamp;
+                            console.log("- ".concat(timestamp, " | ").concat(sender, ": ").concat(content));
+                        });
+                        return [2 /*return*/];
+                }
+            });
+        });
     };
     return Conversation;
 }());
@@ -39,32 +116,148 @@ var Messenger = /** @class */ (function () {
     function Messenger() {
         this.conversations = [];
     }
-    Messenger.prototype.addConversation = function (conversation) {
-        this.conversations.push(conversation);
-    };
-    Messenger.prototype.getConversations = function () {
-        return this.conversations;
-    };
-    // __repr__ equivalent
-    Messenger.prototype.toString = function () {
-        var output = '';
-        this.conversations.forEach(function (conversation) {
-            output += "".concat(conversation.toString(), " \n");
+    Messenger.prototype.loadFromDB = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var snapshot;
+            var _this = this;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, db.collection('conversations').get()];
+                    case 1:
+                        snapshot = _a.sent();
+                        this.conversations = [];
+                        snapshot.forEach(function (doc) {
+                            var data = doc.data();
+                            var convo = new Conversation(data.participants, data.product, doc.id);
+                            _this.conversations.push(convo);
+                        });
+                        return [2 /*return*/];
+                }
+            });
         });
-        return "<Messenger with ".concat(this.conversations.length, " conversations>,\n").concat(output);
+    };
+    Messenger.prototype.printAllMessages = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var _i, _a, convo;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0:
+                        _i = 0, _a = this.conversations;
+                        _b.label = 1;
+                    case 1:
+                        if (!(_i < _a.length)) return [3 /*break*/, 4];
+                        convo = _a[_i];
+                        return [4 /*yield*/, convo.printMessages()];
+                    case 2:
+                        _b.sent();
+                        _b.label = 3;
+                    case 3:
+                        _i++;
+                        return [3 /*break*/, 1];
+                    case 4: return [2 /*return*/];
+                }
+            });
+        });
+    };
+    Messenger.prototype.addConversation = function (participants, product) {
+        return __awaiter(this, void 0, void 0, function () {
+            var docRef, convo;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, db.collection('conversations').add({ participants: participants, product: product })];
+                    case 1:
+                        docRef = _a.sent();
+                        convo = new Conversation(participants, product, docRef.id);
+                        this.conversations.push(convo);
+                        console.log("Conversation added with ID: ".concat(docRef.id));
+                        return [2 /*return*/, convo];
+                }
+            });
+        });
     };
     return Messenger;
 }());
-// Test Example
-var messenger = new Messenger();
-var convo1 = new Conversation(["John", "Kim"], "Book");
-messenger.addConversation(convo1);
-convo1.addMessage("John", "Book please?");
-convo1.addMessage("Kim", "FS $20");
-convo1.addMessage("John", "Great");
-var convo2 = new Conversation(["Bill", "Kate"], "Calculator");
-messenger.addConversation(convo2);
-convo2.addMessage("Bill", "Calc please?");
-convo2.addMessage("Kate", "By calc do you mean calculator");
-convo2.addMessage("Bill", "Yup");
-console.log(messenger.toString());
+// ✅ Manual test code (not called automatically)
+function test() {
+    return __awaiter(this, void 0, void 0, function () {
+        var messenger, convo1, convo2;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    messenger = new Messenger();
+                    return [4 /*yield*/, messenger.loadFromDB()];
+                case 1:
+                    _a.sent();
+                    return [4 /*yield*/, messenger.addConversation(['John', 'Kim'], 'Book')];
+                case 2:
+                    convo1 = _a.sent();
+                    return [4 /*yield*/, convo1.addMessage('John', 'Book please?')];
+                case 3:
+                    _a.sent();
+                    return [4 /*yield*/, convo1.addMessage('Kim', 'FS $20')];
+                case 4:
+                    _a.sent();
+                    return [4 /*yield*/, convo1.addMessage('John', 'Great')];
+                case 5:
+                    _a.sent();
+                    return [4 /*yield*/, messenger.addConversation(['Bill', 'Kate'], 'Calculator')];
+                case 6:
+                    convo2 = _a.sent();
+                    return [4 /*yield*/, convo2.addMessage('Bill', 'Calc please?')];
+                case 7:
+                    _a.sent();
+                    return [4 /*yield*/, convo2.addMessage('Kate', 'By calc do you mean calculator')];
+                case 8:
+                    _a.sent();
+                    return [4 /*yield*/, convo2.addMessage('Bill', 'Yup')];
+                case 9:
+                    _a.sent();
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+function test2() {
+    return __awaiter(this, void 0, void 0, function () {
+        var messenger, convo3;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    messenger = new Messenger();
+                    return [4 /*yield*/, messenger.loadFromDB()];
+                case 1:
+                    _a.sent();
+                    return [4 /*yield*/, messenger.addConversation(['Alice', 'Bob'], 'Headphones')];
+                case 2:
+                    convo3 = _a.sent();
+                    return [4 /*yield*/, convo3.addMessage('Alice', 'Are the headphones still available?')];
+                case 3:
+                    _a.sent();
+                    return [4 /*yield*/, convo3.addMessage('Bob', 'Yes, they are in good condition!')];
+                case 4:
+                    _a.sent();
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+// ✅ Active logic (just loads existing data and prints it)
+function main() {
+    return __awaiter(this, void 0, void 0, function () {
+        var messenger;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    messenger = new Messenger();
+                    return [4 /*yield*/, messenger.loadFromDB()];
+                case 1:
+                    _a.sent();
+                    return [4 /*yield*/, messenger.printAllMessages()];
+                case 2:
+                    _a.sent();
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+main().catch(console.error);
--- backend/messenger.ts
+++ backend/messenger.ts
@@ -1,89 +1,138 @@
+//run this file in terminal 
+// 1st: cd backend
+// 2nd: tsc messenger.ts
+// 3rd: node messenger.js
+
+import * as admin from 'firebase-admin';
+
+const serviceAccount = require('C:/Users/thoma/Documents/ExchangeForStudents/exchange-for-students-firebase-adminsdk-fbsvc-55c7e57c50.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
 class Message {
-    sender: string;
-    content: string;
-    timestamp: string;
-  
-    constructor(sender: string, content: string, timestamp: string) {
-      this.sender = sender;
-      this.content = content;
-      this.timestamp = timestamp;
+  constructor(
+    public sender: string,
+    public content: string,
+    public timestamp: string
+  ) {}
+}
+
+class Conversation {
+  participants: string[];
+  product: string;
+  docId: string;
+
+  constructor(participants: string[], product: string, docId: string) {
+    this.participants = participants;
+    this.product = product;
+    this.docId = docId;
+  }
+
+  async addMessage(sender: string, content: string) {
+    if (this.participants.includes(sender)) {
+      const now = new Date().toISOString();
+      const message = new Message(sender, content, now);
+
+      const messageRef = db.collection('conversations')
+                           .doc(this.docId)
+                           .collection('messages');
+
+      await messageRef.add({
+        sender: message.sender,
+        content: message.content,
+        timestamp: message.timestamp,
+      });
+
+      console.log(`Message added to convo ${this.docId}: ${content}`);
+      return true;
     }
-  
-    // __repr__ equivalent
-    toString(): string {
-      return `Message from ${this.sender} at ${this.timestamp}: ${this.content}`;
+    console.log("Sender not in participants");
+    return false;
+  }
+
+  async printMessages() {
+    const messagesSnapshot = await db.collection('conversations')
+                                     .doc(this.docId)
+                                     .collection('messages')
+                                     .orderBy('timestamp')
+                                     .get();
+
+    console.log(`\nConversation about "${this.product}" between [${this.participants.join(', ')}]:`);
+    messagesSnapshot.forEach(doc => {
+      const { sender, content, timestamp } = doc.data();
+      console.log(`- ${timestamp} | ${sender}: ${content}`);
+    });
+  }
+}
+
+class Messenger {
+  conversations: Conversation[] = [];
+
+  constructor() {}
+
+  async loadFromDB() {
+    const snapshot = await db.collection('conversations').get();
+    this.conversations = []
+    snapshot.forEach(doc => {
+      const data = doc.data();
+      const convo = new Conversation(data.participants, data.product, doc.id);
+      this.conversations.push(convo);
+    });
+  }
+
+  async printAllMessages() {
+    for (const convo of this.conversations) {
+      await convo.printMessages();
     }
   }
 
-class Conversation {    
-    participants: string[];
-    product: string;
-    messages: Message[];
-
-    constructor(participants: string[], product: string) {
-        this.participants = participants;
-        this.product = product;
-        this.messages = [];
-    }
-
-    addMessage(sender: string, content: string): boolean {
-        if (this.participants.includes(sender)) {
-        const now = new Date().toISOString().slice(0, 19).replace('T', ' ');
-        const message = new Message(sender, content, now);
-        this.messages.push(message);
-        return true;
-        }
-        return false;
-    }
-  
-    // __repr__ equivalent
-    toString(): string {
-      let output = '';
-      this.messages.forEach((message) => {
-        output += `${message.toString()} \n`;
-      });
-      return `<Conversation between ${this.participants.join(', ')}>\n${output}`;
-    }
+  async addConversation(participants: string[], product: string): Promise<Conversation> {
+    const docRef = await db.collection('conversations').add({ participants, product });
+    const convo = new Conversation(participants, product, docRef.id);
+    this.conversations.push(convo);
+    console.log(`Conversation added with ID: ${docRef.id}`);
+    return convo;
+  }
 }
-  
-class Messenger {
-    conversations: Conversation[];
 
-    constructor() {
-        this.conversations = [];
-    }
+// ✅ Manual test code (not called automatically)
+async function test() {
+  const messenger = new Messenger();
+  await messenger.loadFromDB();
 
-    addConversation(conversation: Conversation): void {
-        this.conversations.push(conversation);
-    }
+  const convo1 = await messenger.addConversation(['John', 'Kim'], 'Book');
+  await convo1.addMessage('John', 'Book please?');
+  await convo1.addMessage('Kim', 'FS $20');
+  await convo1.addMessage('John', 'Great');
 
-    getConversations(): Conversation[] {
-        return this.conversations;
-    }
-
-    // __repr__ equivalent
-    toString(): string {
-        let output = '';
-        this.conversations.forEach((conversation) => {
-        output += `${conversation.toString()} \n`;
-        });
-        return `<Messenger with ${this.conversations.length} conversations>,\n${output}`;
-    }
+  const convo2 = await messenger.addConversation(['Bill', 'Kate'], 'Calculator');
+  await convo2.addMessage('Bill', 'Calc please?');
+  await convo2.addMessage('Kate', 'By calc do you mean calculator');
+  await convo2.addMessage('Bill', 'Yup');
 }
-  
-// Test Example
-const messenger = new Messenger();
-const convo1 = new Conversation(["John", "Kim"], "Book");
-messenger.addConversation(convo1);
-convo1.addMessage("John", "Book please?");
-convo1.addMessage("Kim", "FS $20");
-convo1.addMessage("John", "Great");
 
-const convo2 = new Conversation(["Bill", "Kate"], "Calculator");
-messenger.addConversation(convo2);
-convo2.addMessage("Bill", "Calc please?");
-convo2.addMessage("Kate", "By calc do you mean calculator");
-convo2.addMessage("Bill", "Yup");
+async function test2() {
+  const messenger = new Messenger();
+  await messenger.loadFromDB();
 
-console.log(messenger.toString());
-  
+  const convo3 = await messenger.addConversation(['Alice', 'Bob'], 'Headphones');
+  await convo3.addMessage('Alice', 'Are the headphones still available?');
+  await convo3.addMessage('Bob', 'Yes, they are in good condition!');
+}
+
+// ✅ Active logic (just loads existing data and prints it)
+async function main() {
+  const messenger = new Messenger();
+  await messenger.loadFromDB();
+  await messenger.printAllMessages();
+  // await console.log("Post Test 2")
+  // await test2()
+  // await messenger.loadFromDB();
+  // await messenger.printAllMessages();
+}
+
+main().catch(console.error);
